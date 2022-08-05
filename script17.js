3202d601a8dd1a9e26d97ba6c395b9cb623aa526
DHINAKARAN P L <108881550+Dhinakaran-PL@users.noreply.github.com>
2022-08-05 18:59:06 +0530

    Add files via upload

---
+++ script17.js
@@ -0,0 +1,122 @@
+
+
+// console.log("one");
+// console.log("two");
+// setTimeout(() => {
+    
+//     console.log("hi man");
+// }, 4000);
+// console.log("three");
+
+
+// document.querySelector("#main").addEventListener("keypress",(event)=>{
+//     let res=event.target.value.length;
+//     //concatenation
+//     let div=document.createElement("div");
+//     div.innerHTML=res;
+//     document.body.append(div);
+// });     
+
+var div=document.createElement("div");
+document.body.append(div)
+
+div.innerHTML=10
+
+let count=10;
+setTimeout(() => {
+    count=count-1
+    div.innerHTML=count
+}, 1000);
+setTimeout(() => {
+    count=count-1
+    div.innerHTML=count
+}, 2000);
+setTimeout(() => {
+    count=count-1
+    div.innerHTML=count
+}, 3000);
+setTimeout(() => {
+    count=count-1
+    div.innerHTML=count
+}, 4000);
+setTimeout(() => {
+    count=count-1
+    div.innerHTML=count
+}, 5000);
+setTimeout(() => {
+    count=count-1
+    div.innerHTML=count
+}, 6000);
+setTimeout(() => {
+    count=count-1
+    div.innerHTML=count
+}, 7000);
+setTimeout(() => {
+    count=count-1
+    div.innerHTML=count
+}, 8000);
+setTimeout(() => {
+    count=count-1
+    div.innerHTML=count
+}, 9000);
+
+setTimeout(() => {
+        div.innerHTML="Happy republic day"
+}, 11000);
+
+
+// let container = document.createElement("div")
+// container.classList.add("container")    
+
+// let span = document.getElementById("span")
+// span.innerHTML=10
+
+// document.body.append(container)
+// let count=10;
+
+// setTimeout(() => {
+//     count=count-1
+//     span.innerHTML=count
+//     setTimeout(() => {
+//         count=count-1
+//         span.innerHTML=count
+//         setTimeout(() => {
+//             count=count-1
+//             span.innerHTML=count
+//             setTimeout(() => {
+//                 count=count-1
+//                 span.innerHTML=count
+//                 setTimeout(() => {
+//                     count=count-1
+//                     span.innerHTML=count
+//                     setTimeout(() => {
+//                         count=count-1
+//                         span.innerHTML=count
+//                         setTimeout(() => {
+//                             count=count-1
+//                             span.innerHTML=count
+//                             setTimeout(() => {
+//                                 count=count-1
+//                                 span.innerHTML=count
+//                                 setTimeout(() => {
+//                                     count=count-1
+//                                     span.innerHTML=count
+//                                     setTimeout(() => {
+//                                         count=count-1
+//                                         span.innerHTML=count
+//                                         setTimeout(() => {
+                                            
+//                                             span.innerHTML="happy independace day"
+//                                         }, 1000);
+//                                     }, 1000);
+//                                 }, 1000);
+//                             }, 1000);
+//                         }, 1000);
+//                     }, 1000);
+//                 }, 1000);
+//             }, 1000);
+//         }, 1000);
+//     }, 1000);
+
+// }, 1000);
+
